fba2e36e5e2aed2a2f36433eacd57f799891f1fc
Jordan Bohm <jordan.bohm@pacer.org>
2013-11-22 14:01:24 -0600

    Initial Commit

---
+++ jquerytest.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+</head>
+
+<body>
+
+
+<div class="superman">
+<p>
+<img src="http://www.pacer.org/bullying/images/partners/spookley-sml.jpg" alt="" width="119" height="71" class="pic" />
+Example text
+</p>
+<p> Example text</p>
+<p> Example text</p>
+</div>
+
+
+<script>
+$('.superman').html($('.superman img').clone());
+</script>
+</body>
+</html>
